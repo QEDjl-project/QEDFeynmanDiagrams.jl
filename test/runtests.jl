444352035800c340ba0b370ec6348ec945b421bb
AntonReinhard <anton.reinhard@proton.me>
2024-11-01 14:34:48 +0100

    Add test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using SafeTestsets
 
+@safetestset "Input Type" begin
+    include("input_type.jl")
+end
+
 @safetestset "Synced Spins and Polarizations" begin
     include("synced_spin_pol.jl")
 end
