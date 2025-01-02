6666fb252b43eace3c7f4478d7f6eff00f1f8519
AntonReinhard <anton.reinhard@proton.me>
2025-01-02 14:37:13 +0100

    Improve manual and number_of_diagrams docs

--- src/diagrams/diagrams.jl
+++ src/diagrams/diagrams.jl
@@ -2,7 +2,17 @@
     number_of_diagrams(proc::AbstractProcessDefinition)
 
 For a given [`QEDbase.AbstractProcessDefinition`](@extref), returns the number of valid
-Feynman diagrams at tree-level.
+Feynman diagrams at tree-level. This is equivalent to 
+
+\$\\frac{(M + 3N - 3)!}{(2N - 1)!} * E! * U! * T!\$\\
+, where\\
+\$M \\dots\$ number of external photons,\\
+\$E \\dots\$ number of electron lines,\\
+\$U \\dots\$ number of muon lines,\\
+\$T \\dots\$ number of tauon lines, and\\
+\$N = E + U + T\$.\\
+
+An electron/muon/tauon "line" is a pair of incoming fermion and outgoing anti-fermion or vice versa.
 """
 function number_of_diagrams(proc::AbstractProcessDefinition)
     M =
